ffc84dbe777256f659262c34656c43bf70280831
Pedro Henrique <pedro.romanowski02@gmail.com>
2025-01-31 10:09:20 -0300

    feat: adicionando um construtor que recebe um UsuarioEntity como parâmetro

--- src/main/java/br/com/alura/codechella/domain/entities/usuario/dto/DtoUsuario.java
+++ src/main/java/br/com/alura/codechella/domain/entities/usuario/dto/DtoUsuario.java
@@ -1,6 +1,11 @@
 package br.com.alura.codechella.domain.entities.usuario.dto;
 
+import br.com.alura.codechella.infra.persistence.UsuarioEntity;
+
 import java.time.LocalDate;
 
-public record DtoUsuario(String cpf, String nome, LocalDate nascimento, String email) {
+public record DtoUsuario(String cpf, String nome, LocalDate nascimento, String email){
+    public DtoUsuario(UsuarioEntity entity){
+        this(entity.getCpf(), entity.getNome(), entity.getNascimento(), entity.getEmail());
+    }
 }
